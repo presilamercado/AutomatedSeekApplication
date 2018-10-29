68f4bbe4523bc89f7c37ce8f359c990a8049b6df
presilamercado <presila.mercado@gmail.com>
2018-10-29 20:23:00 +1300

    First commit
    first commit

---
+++ AutomatedSeekApplication/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomatedSeekApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
+
+
+// TODO LIST
+// 1. Go to seek.co.nz
+// 2. Login with Credentials
+// 3. Filter all IT Jobs within NZ for the day
+// 4. Define Job Filters example ( Tester, Support, Intermediate Developer )
+// 5. Define Exlude Filters example ( Senior, lead )
+// 6. Define Include Companies 
+// 7. Define Exclude Companies
+// 8. Save List in a File ( job_id, description, Company )
+// 8. Create Cover Letter template
+// 9. Apply Job from a save list
+// 10. 
