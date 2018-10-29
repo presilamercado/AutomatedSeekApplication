5f0393865d8b57c91c31dde1d8dbb5def08ed29c
presilamercado <presila.mercado@gmail.com>
2018-10-29 20:45:56 +1300

    Done navigate to login page

--- AutomatedSeekApplication/Program.cs
+++ AutomatedSeekApplication/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 
 namespace AutomatedSeekApplication
 {
@@ -10,21 +13,25 @@ namespace AutomatedSeekApplication
     {
         static void Main(string[] args)
         {
+  
+            IWebDriver driver = new ChromeDriver();
 
+            // And now use this to visit Google
+            driver.Navigate().GoToUrl("https://www.seek.co.nz/sign-in");
         }
     }
 }
 
 
 // TODO LIST
-// 1. Go to seek.co.nz
+// Done 1. Go to seek.co.nz login page
 // 2. Login with Credentials
 // 3. Filter all IT Jobs within NZ for the day
-// 4. Define Job Filters example ( Tester, Support, Intermediate Developer )
+// 4. Define Job Filters example ( Tester, Support, Intermediate, Junior )
 // 5. Define Exlude Filters example ( Senior, lead )
-// 6. Define Include Companies 
+// 6. Define filter include Companies 
 // 7. Define Exclude Companies
-// 8. Save List in a File ( job_id, description, Company )
+// 8. Save List in a File ( JobId, Description, Company, Location, SubmitStatus, DatePosted )
 // 8. Create Cover Letter template
 // 9. Apply Job from a save list
 // 10. 
