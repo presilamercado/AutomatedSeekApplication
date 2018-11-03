4f2648356293da44c0fdc294b2ac4c56bce81dc6
presilamercado <presila.mercado@gmail.com>
2018-11-04 07:18:07 +1300

    Sigin

--- AutomatedSeekApplication/Program.cs
+++ AutomatedSeekApplication/Program.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
+
 using OpenQA.Selenium.Support.UI;
+using System.Threading;
+using Newtonsoft.Json;
+
+
 
 namespace AutomatedSeekApplication
 {
@@ -13,25 +16,10 @@ namespace AutomatedSeekApplication
     {
         static void Main(string[] args)
         {
-  
-            IWebDriver driver = new ChromeDriver();
 
-            // And now use this to visit Google
-            driver.Navigate().GoToUrl("https://www.seek.co.nz/sign-in");
+            Seek seek = new Seek();
+            seek.SignIn();
+
         }
     }
 }
-
-
-// TODO LIST
-// Done 1. Go to seek.co.nz login page
-// 2. Login with Credentials
-// 3. Filter all IT Jobs within NZ for the day
-// 4. Define Job Filters example ( Tester, Support, Intermediate, Junior )
-// 5. Define Exlude Filters example ( Senior, lead )
-// 6. Define filter include Companies 
-// 7. Define Exclude Companies
-// 8. Save List in a File ( JobId, Description, Company, Location, SubmitStatus, DatePosted )
-// 8. Create Cover Letter template
-// 9. Apply Job from a save list
-// 10. 
---
+++ AutomatedSeekApplication/Seek.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace AutomatedSeekApplication
+{
+    class Seek
+    {
+        IWebDriver _driver = new ChromeDriver();
+        public void SignIn()
+        {
+           
+            // Navigate to Sign-in
+            _driver.Navigate().GoToUrl("https://www.seek.co.nz/sign-in");
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+            IWebElement email = _driver.FindElement(By.Id("email"));
+            IWebElement password = _driver.FindElement(By.Id("password"));
+
+
+            // read from a credentials json file
+            JObject credential = new JObject();
+            using (StreamReader file = new StreamReader("credentials.json"))
+            {
+                credential = JObject.Parse(file.ReadToEnd());
+                foreach (var item in credential.Properties())
+                {
+                    if (item.Name == "email")
+                    {
+                        email.SendKeys(item.Value.ToString());
+                    }
+                    if (item.Name == "password")
+                    {
+                        password.SendKeys(item.Value.ToString());
+                    }
+
+                }
+            }
+            password.SendKeys(Keys.Enter);
+        }
+
+        public void SignOut()
+        {   
+            _driver.Close();
+            _driver.Quit();
+        }
+    }
+}
